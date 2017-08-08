53f020c3b7d0df2b52d1415aeb5c1a1692864732
Dominik Schiener <dom@fileyy.com>
2017-08-08 21:42:22 +0200

    New snapshot (8.8.2017)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var request = require('request');
 
 // Declare API endpoint for the IOTA node
-var iotaNode = 'http://localhost:14600'
+var iotaNode = 'http://localhost:15265'
 
 
 var snapshot = function() {
@@ -43,74 +43,7 @@ var snapshot = function() {
 
             console.log("BALANCE CORRECT: ", snapshotBalance === totalSupply);
 
-            // add manual claims to the snapshot
-            addManualClaims(latestState, function(e, updatedState) {
-
-                var nullHash = '999999999999999999999999999999999999999999999999999999999999999999999999999999999';
-
-                var nullHashBalance = updatedState[nullHash];
-                // empty nullHash
-                delete updatedState[nullHash];
-
-                // address controlled by the IOTA Foundation
-                var foundationAddress = 'MCQVDPXBCCANXCFYAYWOZNYKDBTFPSUGPTOFYEMPYVOCKOTDIJLUBBUSQIEHTYUIEORPMIS9ZDXQDSJDR';
-
-                // Send nullHashBalance to the IOTA Foundation Address
-                updatedState[foundationAddress] = nullHashBalance;
-
-                // Case 1
-                //
-                // 500Gi sent to recipient
-                // Reason: Unknowingly sent funds to nullHash. Funds could be easily recovered
-                // via other means
-                var case1recipient = 'M9HNETZSOLPXOFSDELYLAD9Y9YVXAWCYMLCORHGDOYYASJZUPCTGZWWSOHKSFRVNNVR9VXFXMREULIYFJ';
-                var case1Funds = 500000000000;
-                updatedState[foundationAddress] -= case1Funds;
-                updatedState[case1recipient] = updatedState[case1recipient] ? updatedState[case1recipient] += case1Funds : updatedState[case1recipient] = case1Funds;
-
-                // Case 2
-                //
-                // 386697000000 sent to recipient
-                // Reason: Sent funds to old address scheme. Funds could be easily recovered
-                // via other means
-                var case2recipient = 'YX9PFDCREJKYOOQGKXKGPLKOEXTCOXMWCNAMQDFV9WYU9K9OOWPWD9DNBRFMKSJWYTTOGUVBNJXLQM9HF';
-                var case2oldAddress = 'ZQQXBSLFRN9EYOKQUKQ9RGIEFQACIEKUMLTEFSKH9NGSH9VIVJTBGYTCYSBNCJB9QLIVFCPMMTPJVVHOY';
-                var case2Funds = 386697000000
-                updatedState[case2oldAddress] -= case2Funds;
-                updatedState[case2recipient] = updatedState[case2recipient] ? updatedState[case2recipient] += case2Funds : updatedState[case2recipient] = case2Funds;
-
-                delete updatedState[case2oldAddress];
-
-                // Case 3
-                //
-                // 24497000000 sent to recipient
-                // Reason: Sent funds to old address scheme. Funds could be easily recovered
-                // via other means
-                var case3recipient = 'YQXBRVKJOJXWLMI9BUK9XZTOASUTTMKFYCHTQNJSFWCYKSUMB9AUZLZCPU9WFNUOYCYKEB9EXCIN9SEDW';
-                var case3oldAddress = 'MITICUBQBYLYOXIMOBVWSTHEDSOD9OIFNAICTOEONKQTZITZEEYCATHDLHBQULWERMXFLJHALGOG9D9RH';
-                var case3Funds = 24497000000
-                updatedState[case3oldAddress] -= case3Funds;
-                updatedState[case3recipient] = updatedState[case3recipient] ? updatedState[case3recipient] += case3Funds : updatedState[case3recipient] = case3Funds;
-
-                delete updatedState[case3oldAddress];
-
-                // Case 4
-                //
-                // 138129000000 sent to recipient
-                // Reason: Sent funds to old address scheme. Funds could be easily recovered
-                // via other means
-                var case4recipient = 'WP9QVUIREAXFUQH9LNQDCXDJIMWMSDUMJHAHMGCPCFLVKLHGXEIDKKCKDYQH9TXLQUEZVITSTMSMPDWUV';
-                var case4oldAddress = 'HYDMUQPRGGTCSVSGPDCFEZQXKHMZSKIOTJLNOSYKJCRSVMYTVEEF9YTG9LZHUJGJJRUUSPTVFNAPZOMDR';
-                var case4Funds = 138129000000
-                updatedState[case4oldAddress] -= case4Funds;
-                updatedState[case4recipient] = updatedState[case4recipient] ? updatedState[case4recipient] += case4Funds : updatedState[case4recipient] = case4Funds;
-
-                delete updatedState[case4oldAddress];
-
-                // as a final step, compare the two
-                validateSnapshot(updatedState);
-            })
-
+            validateSnapshot(latestState);
 
         } else {
             console.log("COULD NOT PROCESS REQUEST!", error);
@@ -119,58 +52,10 @@ var snapshot = function() {
 
 }
 
-var addManualClaims = function(snapshot, callback) {
-
-    console.log("ADDING MANUAL CLAIMS NOW");
-
-    var manualClaimsList = {
-        'JFKECWKBTQIIBRRZFGUMEROJSSDKLCK9E9CJ9OHQIKKWRYLAZIELNQVHBADXRRNQGSXGESADYMXRANV9K' : '4302318738691',
-        'TWJEAUEYTME9VCCFHNKKIJUKVDVMVHCXCYVJSQVGPMKURYZVJEXZHMOZKAP9RLF9ULN9OODUIZV9ZMBQR' : '333543720000',
-        'BQTVMZFGUABNTZ9HLUAYWKOFVMFPFO9GAGPDUYXDXIRSNEYVBGHLTFHFHPIZXYWVQZJKKO9ZHYAQUEPOE' : '239017429752',
-        'WYVVWUE9RVXJTDBVXRFKYOFMXNUDNSHGTNLKSVVDFAYRCMXOPCRLXANUYMXF9THCVKPDTUXS9HUIQXA9D' : '38913434000',
-        'KFQFBYDCXKMEUPAVSFTNKBYSD9CZVQKI9ZQERAPARLRFQOEM9GCSVSGYMCFNSOKGBUGDMLOSHIKFZBUII' : '12166529484609',
-        'GIOGHJSBRSNITUS9DWSSQRDJKLEIMNOHQBAWTOKJKMHCE9EVXB9AKYYQRTBOSJGYDKHAHJQKLBQUDF9MB' : '1085972906988',
-        'ZZXXCFMPSLMJPNFZZXWICLAYEDHHIAEIJMMTMZLESWHBOAUMCMJL9MFKGPEMMGBOMYSQKGOZII9BFRKHX' : '4780354154102',
-        'UQ9YHOITIZTSVSVEHGYQLVLTLBFLWBM9CQDCRIEVOMFX9SOGSGVQS9LMYUXPCIFIMFR9CIWEGXYKGUHFX' : '2366275306280',
-        'YYVOIYDHRMYWHTCIDVLNWEESKQKHKPPPZIQHKULUC9ZCHXFKYCI9JDQH9DBRKLEEGRYNL9LYMPKPSTAJD' : '4541336446396',
-        'FIEE9MMWGAHWFMGVOLQOSIEMIWEEXUMWZJ9ERQB9AUFFSUTJSATLQXO9BCSFQLVLAXAPEVEEBUCVUFLQX' : '2222864681657',
-        'IXHNYAWKPYTSEECDNONMFAMFXMMDXRMEZHGZKYPAQROAHAXIPKTFUXMXRRESAH9AATWYXTXZMEZFKALAZ' : '1023951425090',
-        'OCWLWWSDFRJYNQKLFZDAKRTFDZMTYFWZPKOUETWWYKSXFMZMRLRBHMPQFKEXPJSAQGHUMZPLNBKVUTYAF' : '38913434000',
-        'ZHFHYNEGUPYVJRTHRSFITNCYYEKLWAHGPMW9VYASNOKZYJLXCUQUTQGODKLGSGNBJLIFWZFLZYZDYRVBD' : '107557843388',
-        'RDZXVRWQBICNM9VVNPERMSGTBFIKGEHRYPHVZNGNTSZVSLKNYZBBSYPKLAMYVQGICKJIWVTGMJVMGPSVU' : '207635968856', 'XYQVEKSSCDKZPXPYIUYF9YAJGKNBXSJUQEONPMCLGWKEPOKAC9JYDVPTZADWRMSCKWEGSYNJUEGKFGTYI': '28351208889433'
-    }
-
-    var addedToSnapshot = 0;
-
-    // This address is owned by the Foundation and is being used to subtract the funds from
-    var addressToSubtract = 'PPLNDRIWPOJZNKEGGPUNFVBEBENBBTQSXKXNOGKNVQBLQNHULLMFRT9RKHOAUCD9JAQNTRFHKEITPVJDC';
-
-    for (var key in manualClaimsList) {
-
-        if (manualClaimsList.hasOwnProperty(key)) {
-
-            var balance = parseInt(manualClaimsList[key]);
-
-            // Add claim to snapshot
-            // If address already existent, simply increment
-            snapshot[key] = snapshot[key] ? snapshot[key] += balance : snapshot[key] = balance;
-            // Subtract claim balance from IOTA Foundation address
-            snapshot[addressToSubtract] -= balance;
-            // Just for logging purposes
-            addedToSnapshot += balance;
-        }
-
-    }
-
-    console.log("ADDED MANUAL CLAIMS: ", addedToSnapshot);
-
-    return callback(null, snapshot);
-
-}
-
 var validateSnapshot = function(latestState) {
 
-    var snapshotUrl = 'https://gist.githubusercontent.com/domschiener/ac11dd4481f940856f07ecf4f2a6b5b9/raw/8872dd1faae3312e1fc953f5539e5cfefc6cd3a5/Snapshot.json'
+    // Snapshot as posted by Paul H (Core Developer of IOTA)
+    var snapshotUrl = 'https://ipfs.io/ipfs/QmRwdX9mwYof1qkMbQgphnsh9kr32itmTtohusWYH1menV'
 
     request(snapshotUrl, function (error, response, body) {
 
